744dc4e46bbd68d1ddb7c053704bbcbc4e8f1e73
Rifat Sadik <rifat.sadik.rs@gmail.com>
2018-04-30 10:32:04 +0600

    added definitions for current_med() and exit_prog() functions. added and defined new function dose_input()

--- medical.cpp
+++ medical.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 //functions for UI options
-void current_med(){};
+int doseinput();        //function to input dosage
+void current_med();     //function to view and review current medication
 void new_med(){};
 void form(){};
-bool exit_prog(){return false;};
+bool exit_prog();       //function to terminate loop
 
 int main(void)
 {
@@ -18,7 +19,7 @@ int main(void)
     {
         opt=0;
 
-        cout<<"Enter patient name: ";
+        cout<<"\nEnter patient name: ";
         cin>>patient;
         cout<<"Select from below:"<<endl
             <<"1. Current Medication"<<endl
@@ -36,4 +37,38 @@ int main(void)
         }
     }
     cout<<endl;
+}
+
+int doseinput()
+{
+    int dose,day;       //variable to store single dose and number of doses per day
+    cout<<"Enter dosage: ";     //single dose input
+    cin>>dose;
+    cout<<"Enter number of doses per day: ";    //number of doses input
+    cin>>day;
+
+    return ((day*10000)+dose);
+}
+
+void current_med()
+{
+    int i,new_dose;     //variable for "if" control statement, and to input new dosage to record
+    cout<<"Current medication and dosage:"<<endl    //User interface
+        <<"Paracetamol 500mg 2 times/day"<<endl
+        <<"1. Agree"<<endl                          //options to confirm or change dosage
+        <<"2. Change dosage"<<endl;
+    cin>>i;
+    if(i==1)
+        cout<<"Verified!"<<endl;
+    else
+    {
+        new_dose = doseinput();
+        cout<<"Dosage changed"<<endl;
+    }
+    return;
+}
+
+bool exit_prog()
+{
+    return false;   //terminating loop and exiting program by making condition false
 }
