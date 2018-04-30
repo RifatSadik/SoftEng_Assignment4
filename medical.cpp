536dbf828a1b0b700943710bfba9f26592b6352c
Hasin Arman Sifat <armansifat@gmail.com>
2018-04-30 10:58:34 +0600

    added function mediinput(). defined functions new_med() and form()

--- medical.cpp
+++ medical.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
+#define  MULT 10000
 using namespace std;
 
 //functions for UI options
 int doseinput();        //function to input dosage
 void current_med();     //function to view and review current medication
-void new_med(){};
-void form(){};
+void new_med();         //function takes input new medication and dose
+void form();            //function lists our medicines before taking input
+string mediinput();     //function to input medication names
 bool exit_prog();       //function to terminate loop
 
 int main(void)
@@ -29,11 +31,20 @@ int main(void)
             <<"Enter option: "<<endl;
         cin>>opt;
 
-        switch(opt){
-        case 1: current_med(); break;
-        case 2: new_med(); break;
-        case 3: form(); break;
-        case 4: on=exit_prog(); break;
+        switch(opt)
+        {
+        case 1:
+            current_med();
+            break;
+        case 2:
+            new_med();
+            break;
+        case 3:
+            form();
+            break;
+        case 4:
+            on=exit_prog();
+            break;
         }
     }
     cout<<endl;
@@ -47,7 +58,7 @@ int doseinput()
     cout<<"Enter number of doses per day: ";    //number of doses input
     cin>>day;
 
-    return ((day*10000)+dose);
+    return ((day*MULT)+dose);
 }
 
 void current_med()
@@ -64,6 +75,74 @@ void current_med()
     {
         new_dose = doseinput();
         cout<<"Dosage changed"<<endl;
+    }
+    return;
+}
+
+string mediinput()
+{
+    string s;   //string variable stores name
+    int choice; //integer variable for selecting options
+    while(1)
+    {
+        cout<<"Enter medication name: ";
+        cin>>s;
+        cout<<"Confirm: "<<s<<endl
+            <<"1. OK \t 2. Change"<<endl;
+        cin>>choice;
+        if(choice==1)
+            break;
+    }
+    return s;
+}
+
+void new_med()
+{
+    string medi;
+    int dose, choice;
+
+    while(1)
+    {
+        medi = mediinput();     //input name using function
+        dose = doseinput();     //input dosage using function
+
+        cout<<"Medication: "<<medi<<endl
+            <<"Dosage: "<<dose%MULT<<"mg "<<dose/MULT<<" times/day"<<endl   //confirming the entered data
+            <<"Confirm?"<<endl
+            <<"1. OK \t 2. Change"<<endl;
+        cin>>choice;
+        if(choice==1)
+            break;
+    }
+    return;
+}
+
+void form()
+{
+    string medi;
+    int dose, choice;
+
+    while(1)
+    {
+        cout<<"Paracetamol"<<endl           //list of medicines
+            <<"Azithromycin"<<endl
+            <<"Ocof"<<endl
+            <<"Alatrol"<<endl
+            <<"Thyronorm"<<endl
+            <<"Thyroxin"<<endl
+            <<"Bisakor 2.5"<<endl
+            <<"Tears Natural Free"<<endl
+            <<"Refresh Tears"<<endl;
+        medi = mediinput();         //input name using function
+        dose = doseinput();         //input dosage using function
+
+        cout<<"Medication: "<<medi<<endl
+            <<"Dosage: "<<dose%MULT<<"mg "<<dose/MULT<<" times/day"<<endl
+            <<"Confirm?"<<endl                      //confirming the entered data
+            <<"1. OK \t 2. Change"<<endl;
+        cin>>choice;
+        if(choice==1)
+            break;
     }
     return;
 }
